a52d818a33aee08ea310500db23da2fdfd048aef
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-03-16 02:33:27 +0530

    kya bolu

---
+++ D1/duplictNumber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class solution
+{
+public:
+    int findDuplicate(int nums[])
+    {
+        int fast = nums[0];
+        int slow = nums[0];
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
+
+int main()
+{
+    solution S;
+    int arr[] = {1, 2, 3, 4, 5, 5};
+    int x = S.findDuplicate(arr);
+    cout << x;
+    return 0;
+}
