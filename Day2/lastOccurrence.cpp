a90cc1dbdca0602604fce040e66dbf5fc27fd573
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-04-23 19:45:43 +0530

    last occurance in sorted array

--- Day2/lastOccurrence.cpp
+++ Day2/lastOccurrence.cpp
@@ -1,1 +1,41 @@
 // Given a sorted array of length n
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int n, int key, vector<int> &v)
+{
+    int start = 0;
+    int end = n - 1;
+    int res = -1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (v[mid] == key)
+        {
+            res = mid;
+            start = mid + 1;
+        }
+        else if (key < v[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n = 7;
+    int key = 13;
+    vector<int> v = {3, 4, 13, 13, 13, 20, 40};
+    cout << solve(n, key, v) << "\n";
+
+    return 0;
+}
