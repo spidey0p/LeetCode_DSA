3e94bdda4d9a71bd1b858de550a89d551502e850
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-04-25 22:06:56 +0530

    rotate array

--- Day2/searchRotateArray.cpp
+++ Day2/searchRotateArray.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int search(vector<int> &nums, int target)
 {
-    int low = 0, high = nums.size() - 1; //<---step 1
+    int low = 0, high = nums.size() - 1;
 
     while (low <= high)
     {
         int mid = (low + high) >> 1;
         if (nums[mid] == target)
-            return mid; // <---step 4
+            return mid;
 
         if (nums[low] <= nums[mid])
-        { //<---step 5
+        {
             if (nums[low] <= target && nums[mid] >= target)
                 high = mid - 1;
             else
