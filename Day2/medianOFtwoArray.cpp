b801d266fa7ab8d728b064a166c601da21fa525f
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-04-28 23:08:43 +0530

    Median of Two Sorted Arrays of different sizes

--- Day2/medianOFtwoArray.cpp
+++ Day2/medianOFtwoArray.cpp
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-float median(int num 1 [], int num2[], int m, int n)
+float median(int nums1[], int nums2[], int m, int n)
 {
-    if (m > n)
-        return median(nums2, nums1, n, m); // ensuring that binary search happens on minimum size array
-
-    int low = 0, high = m, medianPos = ((m + n) + 1) / 2;
-    while (low <= high)
+    int finalArray[n + m];
+    int i = 0, j = 0, k = 0;
+    while (i < m && j < n)
     {
-        int cut1 = (low + high) >> 1;
-        int cut2 = medianPos - cut1;
-
-        int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-        int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-        int r1 = (cut1 == m) ? INT_MAX : nums1[cut1];
-        int r2 = (cut2 == n) ? INT_MAX : nums2[cut2];
-
-        if (l1 <= r2 && l2 <= r1)
+        if (nums1[i] < nums2[j])
         {
-            if ((m + n) % 2 != 0)
-                return max(l1, l2);
-            else
-                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            finalArray[k++] = nums1[i++];
         }
-        else if (l1 > r2)
-            high = cut1 - 1;
         else
-            low = cut1 + 1;
+        {
+            finalArray[k++] = nums2[j++];
+        }
     }
-    return 0.0;
+    if (i < m)
+    {
+        while (i < m)
+            finalArray[k++] = nums1[i++];
+    }
+    if (j < n)
+    {
+        while (j < n)
+            finalArray[k++] = nums2[j++];
+    }
+    n = n + m;
+    if (n % 2 == 1)
+        return finalArray[((n + 1) / 2) - 1];
+    else
+        return ((float)finalArray[(n / 2) - 1] + (float)finalArray[(n / 2)]) / 2;
 }
 
 int main()
@@ -38,7 +39,7 @@ int main()
     int nums2[] = {2, 3, 6, 15};
     int m = sizeof(nums1) / sizeof(nums1[0]);
     int n = sizeof(nums2) / sizeof(nums2[0]);
-    cout << "The Median of two sorted arrays is" << fixed << setprecision(5)
+    cout << "The median of two sorted array is " << fixed << setprecision(5)
          << median(nums1, nums2, m, n);
     return 0;
 }
