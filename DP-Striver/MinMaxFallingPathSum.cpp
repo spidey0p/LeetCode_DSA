e432141cd5f78db36a02124a7a4c01c784c7a6ae
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-19 13:30:57 +0530

    Minimum/Maximum Falling Path Sum

--- DP-Striver/MinMaxFallingPathSum.cpp
+++ DP-Striver/MinMaxFallingPathSum.cpp
@@ -1,46 +1,99 @@
 
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
+// using namespace std;
+
+// int getMaxUtil(int i, int j, int m, vector<vector<int>> &grid, vector<vector<int>> &dp)
+// {
+
+//     // Base Conditions
+//     if (j < 0 || j >= m)
+//         return -1e9;
+//     if (i == 0)
+//         return grid[0][j];
+
+//     if (dp[i][j] != -1)
+//         return dp[i][j];
+
+//     int up = grid[i][j] + getMaxUtil(i - 1, j, m, grid, dp);
+//     int leftDiagonal = grid[i][j] + getMaxUtil(i - 1, j - 1, m, grid, dp);
+//     int rightDiagonal = grid[i][j] + getMaxUtil(i - 1, j + 1, m, grid, dp);
+
+//     return dp[i][j] = max(up, max(leftDiagonal, rightDiagonal));
+// }
+
+// int getMaxPathSum(vector<vector<int>> &grid)
+// {
+
+//     int n = grid.size();
+//     int m = grid[0].size();
+
+//     vector<vector<int>> dp(n, vector<int>(m, -1));
+
+//     int maxi = INT_MIN;
+
+//     for (int j = 0; j < m; j++)
+//     {
+//         int ans = getMaxUtil(n - 1, j, m, grid, dp);
+//         maxi = max(maxi, ans);
+//     }
+
+//     return maxi;
+// }
+
+// int main()
+// {
+
+//     vector<vector<int>> grid{{1, 2, 10, 4},
+//                              {100, 3, 2, 1},
+//                              {1, 1, 20, 2},
+//                              {1, 2, 2, 1}};
+
+//     cout << getMaxPathSum(grid);
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int getMaxUtil(int i, int j, int m, vector<vector<int>> &grid, vector<vector<int>> &dp)
+int pathSum(vector<vector<int>> &grid)
 {
-
-    // Base Conditions
-    if (j < 0 || j >= m)
-        return -1e9;
-    if (i == 0)
-        return grid[0][j];
-
-    if (dp[i][j] != -1)
-        return dp[i][j];
-
-    int up = grid[i][j] + getMaxUtil(i - 1, j, m, grid, dp);
-    int leftDiagonal = grid[i][j] + getMaxUtil(i - 1, j - 1, m, grid, dp);
-    int rightDiagonal = grid[i][j] + getMaxUtil(i - 1, j + 1, m, grid, dp);
-
-    return dp[i][j] = max(up, max(leftDiagonal, rightDiagonal));
-}
-
-int getMaxPathSum(vector<vector<int>> &grid)
-{
-
     int n = grid.size();
     int m = grid[0].size();
-
-    vector<vector<int>> dp(n, vector<int>(m, -1));
-
-    int maxi = INT_MIN;
+    vector<vector<int>> dp(n, vector<int>(m, 0));
 
     for (int j = 0; j < m; j++)
     {
-        int ans = getMaxUtil(n - 1, j, m, grid, dp);
-        maxi = max(maxi, ans);
+        dp[0][j] = grid[0][j];
     }
 
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int up = grid[i][j] + dp[i - 1][j];
+
+            int leftDig = grid[i][j];
+            if (j - 1 >= 0)
+                leftDig += dp[i - 1][j - 1];
+            else
+                leftDig += -1e9;
+
+            int rightDig = grid[i][j];
+            if (j + 1 < m)
+                rightDig += dp[i - 1][j + 1];
+            else
+                rightDig += -1e9;
+
+            dp[i][j] = max(up, max(leftDig, rightDig));
+        }
+    }
+    int maxi = INT_MIN;
+    for (int j = 0; j < m; j++)
+    {
+        maxi = max(maxi, dp[n - 1][j]);
+    }
     return maxi;
 }
-
 int main()
 {
 
@@ -49,5 +102,5 @@ int main()
                              {1, 1, 20, 2},
                              {1, 2, 2, 1}};
 
-    cout << getMaxPathSum(grid);
+    cout << pathSum(grid);
 }
