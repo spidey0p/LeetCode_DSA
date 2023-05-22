96f0977e86e1bff35204ca6102dd55ec6582a6f4
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-22 13:12:10 +0530

    Ninja and his friends

--- DP-Striver/NinjaAndFriends.cpp
+++ DP-Striver/NinjaAndFriends.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int maxChoco(int i, int j1, int j2, int n, int m, vector<vector<int>> &grid, vector<vector<vector<int>>> &dp)
+{
+    if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m)
+        return -1e9;
+
+    if (i == n - 1)
+    {
+        if (j1 == j2)
+            return grid[i][j1];
+        else
+            return grid[i][j1] + grid[i][j2];
+    }
+}
+
 int main()
 {
 }
