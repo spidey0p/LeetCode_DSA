c2a0282406603a99b9b72b2cd4fb470b611739d1
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-25 21:25:27 +0530

    Partition Set Into 2 Subsets With Min Absolute Sum Diff

--- DP-Striver/partitionSet.cpp
+++ DP-Striver/partitionSet.cpp
@@ -1,61 +1,120 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool subSet(int ind, int target, vector<int> &arr, vector<vector<int>> &dp)
+// {
+//     if (target == 0)
+//         return dp[ind][target] = true;
+
+//     if (ind == 0)
+//         return dp[ind][target] = target;
+
+//     if (dp[ind][target] != -1)
+//         return dp[ind][target];
+
+//     bool notTake = subSet(ind - 1, target, arr, dp);
+//     bool take = false;
+
+//     if (arr[ind] <= target)
+//     {
+//         take = subSet(ind - 1, target - arr[ind], arr, dp);
+//     }
+//     return dp[ind][target] = notTake || take;
+// }
+
+// int minSubSetDiff(vector<int> &arr, int n)
+// {
+//     int totSum = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         totSum += arr[i];
+//     }
+
+//     vector<vector<int>> dp(n, vector<int>(totSum + 1, -1));
+
+//     for (int i = 0; i <= totSum; i++)
+//     {
+//         bool dummy = subSet(n - 1, i, arr, dp);
+//     }
+
+//     int max = 1e9;
+//     for (int i = 0; i <= totSum; i++)
+//     {
+//         if (dp[n - 1][i] == true)
+//         {
+//             int diff = abs(i - (totSum - i));
+//             max = min(max, diff);
+//         }
+//     }
+//     return max;
+// }
+// int main()
+// {
+
+//     vector<int> arr = {1, 2, 3, 4};
+
+//     int n = arr.size();
+
+//     cout << "The minimum absolute difference is: " << minSubSetDiff(arr, n);
+// }
+
+// /*The minimum absolute difference is: 0*/
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
-bool subSet(int ind, int target, vector<int> &arr, vector<vector<int>> &dp)
-{
-    if (target == 0)
-        return dp[ind][target] = true;
-
-    if (ind == 0)
-        return dp[ind][target] = target;
-
-    if (dp[ind][target] != -1)
-        return dp[ind][target];
-
-    bool notTake = subSet(ind - 1, target, arr, dp);
-    bool take = false;
-
-    if (arr[ind] <= target)
-    {
-        take = subSet(ind - 1, target - arr[ind], arr, dp);
-    }
-    return dp[ind][target] = notTake || take;
-}
-
-int minSubSetDiff(vector<int> &arr, int n)
+int minSubsetSumDifference(vector<int> &arr, int n)
 {
     int totSum = 0;
+
     for (int i = 0; i < n; i++)
     {
         totSum += arr[i];
     }
 
-    vector<vector<int>> dp(n, vector<int>(totSum + 1, -1));
+    vector<vector<bool>> dp(n, vector<bool>(totSum + 1, false));
 
-    for (int i = 0; i <= totSum; i++)
+    for (int i = 0; i < n; i++)
     {
-        bool dummy = subSet(n - 1, i, arr, dp);
+        dp[i][0] = true;
     }
 
-    int max = 1e9;
+    if (arr[0] <= totSum)
+        dp[0][totSum] = true;
+
+    for (int ind = 1; ind < n; ind++)
+    {
+        for (int target = 1; target <= totSum; target++)
+        {
+
+            bool notTaken = dp[ind - 1][target];
+
+            bool taken = false;
+            if (arr[ind] <= target)
+                taken = dp[ind - 1][target - arr[ind]];
+
+            dp[ind][target] = notTaken || taken;
+        }
+    }
+
+    int mini = 1e9;
     for (int i = 0; i <= totSum; i++)
     {
         if (dp[n - 1][i] == true)
         {
             int diff = abs(i - (totSum - i));
-            max = min(max, diff);
+            mini = min(mini, diff);
         }
     }
-    return max;
+    return mini;
 }
+
 int main()
 {
 
     vector<int> arr = {1, 2, 3, 4};
-
     int n = arr.size();
 
-    cout << "The minimum absolute difference is: " << minSubSetDiff(arr, n);
+    cout << "The minimum absolute difference is: " << minSubsetSumDifference(arr, n);
 }
-
-/*The minimum absolute difference is: 0*/
