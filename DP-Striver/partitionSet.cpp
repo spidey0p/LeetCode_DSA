96f01d1b997da00ab23b8ee63fa145aa8694f497
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-25 14:50:03 +0530

    Partition Set Into 2 Subsets With Min Absolute Sum Diff

---
+++ DP-Striver/partitionSet.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool subSet(int ind, int tar, vector<int> &arr, vector<vector<int>> &dp)
+{
+    if (tar == 0)
+        return dp[ind][tar] = true;
+}
