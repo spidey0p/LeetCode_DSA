0246c717f206430e368dbbfa22ff839695307617
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-26 20:00:58 +0530

    Graph representation in adjacency list for undirected graph

--- Graph-Striver/representG.cpp
+++ Graph-Striver/representG.cpp
@@ -1,4 +1,5 @@
 // graph representation in Adjacency matrix form in c++
+/*
 #include <iostream>
 using namespace std;
 int main()
@@ -12,6 +13,27 @@ int main()
         cin >> u >> v;
         adj[u][v] = 1;
         adj[v][u] = 1; // in cace of directed graph this statement will removed;
+    }
+    return 0;
+}*/
+
+// Graph representation in adjacency list for undirected graph
+// time complexity: O(2E)
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
     return 0;
 }
