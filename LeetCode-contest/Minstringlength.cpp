2f45791e8646ee3b89afe0d4d79e08d55128efb3
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-27 21:49:41 +0530

    min string length after removing

---
+++ LeetCode-contest/Minstringlength.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minLength(string s)
+{
+    stack<char> stk;
+
+    for (char ch : s)
+    {
+        if (!stk.empty() && (stk.top() == 'A' && ch == 'B' || stk.top() == 'C' && ch == 'D'))
+        {
+            stk.pop();
+        }
+        else
+        {
+            stk.push(ch);
+        }
+    }
+    return stk.size();
+}
+
+int main()
+{
+    string s;
+    cout << "Enter the string:";
+    cin >> s;
+
+    int length = minLength(s);
+    cout << "Min Length is: " << length << endl;
+}
