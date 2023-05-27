cde4c4fcc3b7739a160ea4efad9bfbf3d8e9dc94
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-27 10:48:00 +0530

    graph representation in c++

--- Graph-Striver/representG.cpp
+++ Graph-Striver/representG.cpp
@@ -1,39 +1,39 @@
-// graph representation in Adjacency matrix form in c++
-/*
-#include <iostream>
-using namespace std;
-int main()
-{
-    int n, m;
-    cin >> n >> m;
-    int adj[n + 1][m + 1];
-    for (int i = 0; i < m; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        adj[u][v] = 1;
-        adj[v][u] = 1; // in cace of directed graph this statement will removed;
-    }
-    return 0;
-}*/
+// // graph representation in Adjacency matrix form in c++
+// /*
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n, m;
+//     cin >> n >> m;
+//     int adj[n + 1][m + 1];
+//     for (int i = 0; i < m; i++)
+//     {
+//         int u, v;
+//         cin >> u >> v;
+//         adj[u][v] = 1;
+//         adj[v][u] = 1; // in cace of directed graph this statement will removed;
+//     }
+//     return 0;
+// }*/
 
-// Graph representation in adjacency list for undirected graph
-// time complexity: O(2E)
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int n, m;
-    cin >> n >> m;
+// // Graph representation in adjacency list for undirected graph
+// // time complexity: O(2E)
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n, m;
+//     cin >> n >> m;
 
-    vector<int> adj[n + 1];
-    for (int i = 0; i < m; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u); // in case of directed graph this line will remove
-    }
-    return 0;
-}
+//     vector<int> adj[n + 1];
+//     for (int i = 0; i < m; i++)
+//     {
+//         int u, v;
+//         cin >> u >> v;
+//         adj[u].push_back(v);
+//         adj[v].push_back(u); // in case of directed graph this line will remove
+//     }
+//     return 0;
+// }
