81ebfed48ea76cc907918c880d62080fee5eed7e
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-05-30 11:23:43 +0530

    sub set sum equals to target (Tabulation method)

--- DP-Striver/SubSetSum.cpp
+++ DP-Striver/SubSetSum.cpp
@@ -1,41 +1,71 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool subSet(int ind, int target, vector<int> &arr, vector<vector<int>> &dp)
+// {
+//     if (target == 0)
+//         return true;
+
+//     if (ind == 0)
+//         return arr[0] == target;
+
+//     if (dp[ind][target] != -1)
+//         return dp[ind][target];
+
+//     bool notTaken = subSet(ind - 1, target, arr, dp);
+
+//     bool taken = false;
+//     if (arr[ind] <= target)
+//         taken = subSet(ind - 1, target - arr[ind], arr, dp);
+
+//     return dp[ind][target] = notTaken || taken;
+// }
+// bool subSetSum(int n, int k, vector<int> &arr)
+// {
+
+//     vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+//     return subSet(n - 1, k, arr, dp);
+// }
+// int main()
+// {
+//     vector<int> arr = {1, 2, 3, 4};
+//     int k = 4;
+//     int n = arr.size();
+
+//     if (subSetSum(n, k, arr))
+//     {
+//         cout << "Subset with given target found";
+//     }
+//     else
+//         cout << "Subset with given target not fount";
+// }/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-bool subSet(int ind, int target, vector<int> &arr, vector<vector<int>> &dp)
+bool subSet(int n, int k, vector<int> &arr)
 {
-    if (target == 0)
-        return true;
+    vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
 
-    if (ind == 0)
-        return arr[0] == target;
-
-    if (dp[ind][target] != -1)
-        return dp[ind][target];
-
-    bool notTaken = subSet(ind - 1, target, arr, dp);
-
-    bool taken = false;
-    if (arr[ind] <= target)
-        taken = subSet(ind - 1, target - arr[ind], arr, dp);
-
-    return dp[ind][target] = notTaken || taken;
-}
-bool subSetSum(int n, int k, vector<int> &arr)
-{
-
-    vector<vector<int>> dp(n, vector<int>(k + 1, -1));
-    return subSet(n - 1, k, arr, dp);
-}
-int main()
-{
-    vector<int> arr = {1, 2, 3, 4};
-    int k = 4;
-    int n = arr.size();
-
-    if (subSetSum(n, k, arr))
+    for (int i = 0; i < n; i++)
     {
-        cout << "Subset with given target found";
+        dp[i][0] = true;
     }
-    else
-        cout << "Subset with given target not fount";
+    if (arr[0] <= k)
+        dp[0][arr[0]] = true;
+
+    for (int ind = 1; ind < n; ind++)
+    {
+        for (int target = 1; target <= k; target++)
+        {
+            bool notTake = dp[ind - 1][target];
+
+            bool take = false;
+            if (arr[ind] <= target)
+                take = dp[ind - 1][target - arr[ind]];
+
+            dp[ind][target] = notTake || take;
+        }
+    }
+    return dp[n - 1][k];
 }
