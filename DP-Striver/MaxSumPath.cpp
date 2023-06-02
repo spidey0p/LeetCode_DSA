c35f50863354b3acfe7d6a38de6b3fb5f6faa362
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-02 10:21:39 +0530

    Maximum sum of non-adjacent elements

---
+++ DP-Striver/MaxSumPath.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSum(int ind, vector<int> &arr, vector<int> &dp)
+{
+    if (dp[ind] != -1)
+        return dp[ind];
+
+    if (ind == 0)
+        return arr[ind];
+    if (ind < 0)
+        return 0;
+
+    int pick = arr[ind] + maxSum(ind - 2, arr, dp);
+    int notPick = 0 + maxSum(ind - 1, arr, dp);
+
+    return dp[ind] = max(pick, notPick);
+}
+
+int maxSumPath(int n, vector<int> &arr)
+{
+    vector<int>(n, -1);
+    return maxSum(n - 1, arr, arr);
+}
+int main()
+{
+    vector<int> arr{2, 1, 4, 9};
+    int n = arr.size();
+    cout << maxSumPath(n, arr) << endl;
+}
