23294e9a50ed73455e20040f8abc7fb1b2385b26
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-04 10:48:25 +0530

    count and say string operation

--- LeetCode-contest/CountAndSay.cpp
+++ LeetCode-contest/CountAndSay.cpp
@@ -3,7 +3,31 @@ using namespace std;
 
 string countAndSay(int n)
 {
-    for (int i = 1; i <= n; i++)
+    if (n == 1)
+        return "1";
+    if (n == 2)
+        return "11";
+
+    string str = "11";
+
+    for (int i = 3; i <= n; i++)
     {
+        string temp = "";
+        str = str + "&";
+        int cnt = 1;
+
+        for (int j = 0; j < str.length(); j++)
+        {
+            if (str[j] != str[j - 1])
+            {
+                temp = temp + to_string(cnt);
+                temp = temp + str[j - 1];
+                cnt = 1;
+            }
+            else
+                cnt++;
+        }
+        str = temp;
     }
+    return str;
 }
