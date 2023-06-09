30d7f20a2e6bba91ba80f6b5046191bdb4e5b74a
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-09 11:13:47 +0530

    Find Smallest Letter Greater Than Target by Binary search

--- DailyChallenge/090623.cpp
+++ DailyChallenge/090623.cpp
@@ -3,16 +3,39 @@ using namespace std;
 
 char nextGreatestLetter(vector<char> &letters, char target)
 {
-    char answer = letters[0];
-    for (int i = 0; i < letters.size(); i++)
+    // char answer = letters[0];
+    // for (int i = 0; i < letters.size(); i++)
+    // {
+    //     if (letters[i] > target)
+    //     {
+    //         answer = letters[i];
+    //         break;
+    //     }
+    // }
+    // return answer;
+
+    // by Binary search
+
+    int low = 0;
+    int high = letters.size() - 1;
+
+    while (low < high)
     {
-        if (letters[i] > target)
+        int mid = (low + high) / 2;
+        if (letters[mid] <= target)
         {
-            answer = letters[i];
-            break;
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid;
         }
     }
-    return answer;
+    if (letters[low] > target)
+    {
+        return letters[low];
+    }
+    return letters[0];
 }
 
 int main()
