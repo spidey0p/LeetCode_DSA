1df1cb45a36a60225177bfabc6363b5a0c1829eb
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-09 12:04:56 +0530

    Semi-Ordered Permutation

---
+++ LeetCode-contest/semi_order.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int semiOrderedPermutation(vector<int> &nums)
+{
+    int n = nums.size();
+    int min = 0, max = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == 1)
+            min = i;
+        if (nums[i] == n)
+            max = i;
+    }
+    if (min < max)
+        return (min + n - max - 1);
+    return (min + n - max - 1) - 1;
+}
+int main()
+{
+}
