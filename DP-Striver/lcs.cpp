ccc5c80bb88aeacd047d74d692e951b18fc8175d
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-11 09:06:23 +0530

    lcs

--- DP-Striver/lcs.cpp
+++ DP-Striver/lcs.cpp
@@ -1,16 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-int myLove(string &str1, string &str2, int ind1, int ind2, vector<vector<int>> &dp)
-{
-    if (ind1 < 0 && ind2 < 0)
-        return 0;
-    if (dp[ind1][ind2] != -1)
-        return dp[ind1][ind2];
-    if (str1[ind1] == str2[ind2])
-        return dp[ind1][ind2] = 1 + myLove(str1, str2, ind1 - 1, ind2 - 1, dp);
-    else
-        return dp[ind1][ind2] = 0 + max(myLove(str1, str2, ind1 - 1, ind2, dp), myLove(str1, str2, ind1, ind2 - 1, dp));
-}
 
 int longestCommonSubsequence(string text1, string text2)
 {
@@ -18,5 +7,26 @@ int longestCommonSubsequence(string text1, string text2)
     int m = text2.size();
 
     vector<vector<int>> dp(n, vector<int>(m, -1));
-    return myLove(text1, text2, n - 1, m - 1, dp);
+    for (int i = 0; i <= n; i++)
+    {
+        dp[i][0] = 0;
+    }
+    for (int i = 0; i <= m; i++)
+    {
+        dp[0][i] = 0;
+    }
+
+    for (int ind1 = 1; ind1 <= n; ind1++)
+    {
+        for (int ind2 = 1; ind2 <= m; ind2++)
+        {
+            if (text1[ind1 - 1] = text2[ind2 - 1])
+            {
+                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+            }
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+        }
+    }
+    return dp[n][m];
 }
