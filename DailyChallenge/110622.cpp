ac7277cf660b13e3e781a5db2efdfefc28330f63
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-12 21:20:58 +0530

    daily challenge

---
+++ DailyChallenge/110622.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class SnapshotArray
+{
+public:
+    SnapshotArray(int length)
+    {
+    }
+
+    void set(int index, int val)
+    {
+    }
+
+    int snap()
+    {
+    }
+
+    int get(int index, int snap_id)
+    {
+    }
+};
