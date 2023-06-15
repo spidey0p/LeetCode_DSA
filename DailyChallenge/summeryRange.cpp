8504ffc498c585f61fe6cffe5e52c712a808140f
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-15 23:26:06 +0530

    summery range

--- DailyChallenge/summeryRange.cpp
+++ DailyChallenge/summeryRange.cpp
@@ -24,3 +24,30 @@ vector<string> summaryRanges(vector<int> &nums)
     }
     return rng;
 }
+
+int main()
+{
+    // Test case 1
+    vector<int> nums1 = {0, 1, 2, 4, 5, 7};
+    vector<string> result1 = summaryRanges(nums1);
+
+    cout << "Result 1: ";
+    for (const auto &range : result1)
+    {
+        cout << range << " ";
+    }
+    cout << endl;
+
+    // Test case 2
+    vector<int> nums2 = {1, 3, 4, 6, 8, 9};
+    vector<string> result2 = summaryRanges(nums2);
+
+    cout << "Result 2: ";
+    for (const auto &range : result2)
+    {
+        cout << range << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
