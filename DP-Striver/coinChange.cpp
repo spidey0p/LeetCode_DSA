674a3b8b7f355b62e8e0e9d29a8bd721dd7aff97
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-16 22:10:59 +0530

    coin change

--- DP-Striver/coinChange.cpp
+++ DP-Striver/coinChange.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int coinChange(vector<int> &coins, int amount)
+int coinChange(vector<int> &coins, int T)
 {
+    int n = coins.size();
+
+    vector<int> prev(T + 1, 0), cur(T + 1, 0);
 }
