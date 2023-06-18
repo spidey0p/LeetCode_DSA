fc9c1a324f1ac76c3004d7f9060cb1b9a76c7315
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-18 08:45:23 +0530

    total sum

---
+++ LeetCode-contest/Total.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int distanceTraveled(int mainTank, int additionalTank)
+{
+
+    int i = 1, ans = 0;
+    while (i <= mainTank)
+    {
+
+        ans += 10;
+
+        if (i % 5 == 0 && additionalTank > 0)
+        {
+            mainTank++;
+            additionalTank--;
+        }
+
+        i++;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int x = 5;
+    int y = 10;
+    cout << distanceTraveled(x, y);
+}
