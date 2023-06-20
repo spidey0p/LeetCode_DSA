6357733542dd6b413d07efe9c8a6bc315ea4fe18
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-20 23:32:55 +0530

    Frequency of the Most Frequent Element

--- D1/freOFmost.cpp
+++ D1/freOFmost.cpp
@@ -3,4 +3,24 @@ using namespace std;
 
 int maxFrequency(vector<int> &nums, int k)
 {
+    sort(nums.begin(), nums.end());
+
+    int pos = 0;
+    long result = 0;
+    long window_size = 0;
+    long window_sum = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        window_sum += nums[i];
+        window_size = i - pos + 1;
+
+        while ((nums[i] * window_size - window_sum) > k)
+        {
+            window_sum -= nums[pos++];
+            window_size--;
+        }
+        result = max(result, window_size);
+    }
+    return result;
 }
