92fed7320ffed4b68163ce7e42cb1abe73bb2e06
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-25 23:16:54 +0530

    level order traversal

--- BT-Striver/levelOrderTravs.cpp
+++ BT-Striver/levelOrderTravs.cpp
@@ -21,6 +21,7 @@ vector<vector<int>> levelOrder(TreeNode *root)
         int size = q.size();
 
         vector<int> level;
+
         for (int i = 0; i < size; i++)
         {
             TreeNode *node = q.front();
