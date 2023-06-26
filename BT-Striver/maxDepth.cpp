8e0a490a453080480edcedadd9be8bee6fad1913
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-26 21:19:15 +0530

    Maximum Depth of Binary Tree

---
+++ BT-Striver/maxDepth.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+int maxDepth(TreeNode *root)
+{
+    if (root == NULL)
+        return 0;
+
+    int lft = maxDepth(root->left);
+    int rgt = maxDepth(root->right);
+
+    return 1 + max(lft, rgt);
+}
