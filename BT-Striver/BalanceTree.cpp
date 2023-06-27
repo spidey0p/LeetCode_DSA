3004b11b0fb4f089d9f3cca8debd1813d47b4533
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-27 23:28:26 +0530

    Check if the Binary Tree is Balanced Binary Tree

--- BT-Striver/BalanceTree.cpp
+++ BT-Striver/BalanceTree.cpp
@@ -7,6 +7,26 @@ struct TreeNode
     TreeNode *left, *right;
 };
 
+int dfsHeight(TreeNode *root)
+{
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = dfsHeight(root->left);
+    if (leftHeight == -1)
+        return -1;
+
+    int rightHeight = dfsHeight(root->right);
+    if (rightHeight == -1)
+        return -1;
+
+    if (abs(leftHeight - rightHeight) > 1)
+        return -1;
+
+    return max(leftHeight, rightHeight) + 1;
+}
+
 bool isBalanced(TreeNode *root)
 {
+    return dfsHeight(root) != -1;
 }
