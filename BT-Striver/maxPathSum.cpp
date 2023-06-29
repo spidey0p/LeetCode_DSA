00a5183fad4f6418c6986e2934ba88c915ce140a
Jyotiraditya Biswal <98413673+spidey0p@users.noreply.github.com>
2023-06-29 20:40:15 +0530

    binary tree maximum path

--- BT-Striver/maxPathSum.cpp
+++ BT-Striver/maxPathSum.cpp
@@ -4,8 +4,22 @@ using namespace std;
 struct TreeNode
 {
     int val;
-    int *left, *right;
+    TreeNode *left, *right;
 };
+int findMaxPathSum(TreeNode *root, int &maxi)
+{
+    if (root == NULL)
+        return 0;
+
+    int lp = max(0, findMaxPathSum(root->left, maxi));
+    int rp = max(0, findMaxPathSum(root->right, maxi));
+    int data = root->val;
+    maxi = max(maxi, (lp + rp) + data);
+    return max(lp, rp) + data;
+}
 int maxPathSum(TreeNode *root)
 {
+    int maxi = INT_MIN;
+    findMaxPathSum(root, maxi);
+    return maxi;
 }
