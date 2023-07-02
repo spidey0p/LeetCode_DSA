0b7e5f303f8fc1091d840b4ae74330b41db7307d
spidey0p <jyotiradityabiswal64@gmail.com>
2023-07-02 21:14:48 +0530

    Check if two trees are identical

---
+++ BT-Striver/identicalTrees.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left, *right;
+};
+bool isSameTree(TreeNode *p, TreeNode *q)
+{
+    if (p == NULL && q == NULL)
+        return true;
+    else if (p == NULL || q == NULL)
+        return false;
+
+    return ((p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+}
