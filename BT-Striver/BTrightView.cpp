c2ec5d08919e0c84a4fd4a5e35ac6aeaa7fb9d56
spidey0p <jyotiradityabiswal64@gmail.com>
2023-07-04 00:44:27 +0530

    199. Binary Tree Right Side View

--- BT-Striver/BTrightView.cpp
+++ BT-Striver/BTrightView.cpp
@@ -9,4 +9,27 @@ struct TreeNode
 vector<int> rightSideView(TreeNode *root)
 {
     vector<int> ans;
+    queue<TreeNode *> q;
+    if (root == NULL)
+        return ans;
+    q.push(root);
+    while (!q.empty())
+    {
+        int size = q.size();
+        if (size == 0)
+            return ans;
+        vector<int> data(size);
+        while (size--)
+        {
+            TreeNode *temp = q.front();
+            q.pop();
+            data.push_back(temp->val);
+
+            if (temp->left != NULL)
+                q.push(temp->left);
+            if (temp->right != NULL)
+                q.push(temp->right);
+        }
+        ans.push_back(data.back());
+    }
 }
