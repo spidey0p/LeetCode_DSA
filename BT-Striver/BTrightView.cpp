8b1860ebd6f4dcf26456295f34d405d5295b207b
spidey0p <jyotiradityabiswal64@gmail.com>
2023-07-03 23:17:04 +0530

    199. Binary Tree Right Side View

---
+++ BT-Striver/BTrightView.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left, *right;
+};
+vector<int> rightSideView(TreeNode *root)
+{
+    vector<int> ans;
+}
