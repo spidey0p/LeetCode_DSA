2f1d62f6392a5fb776a5d9293c39f0d3c7af9db9
spidey0p <jyotiradityabiswal64@gmail.com>
2023-07-18 21:36:33 +0530

    cuont good numbers

--- D1/CntGdNum.cpp
+++ D1/CntGdNum.cpp
@@ -8,6 +8,7 @@ long long power(long long x, long long n)
         return 1;
     long long ans = power(x, n / 2);
     ans *= ans;
+
     ans %= mod;
 
     if (n % 2)
@@ -19,6 +20,7 @@ long long power(long long x, long long n)
 int countGoodNumbers(long long n)
 {
     long long odd = n / 2;
+
     long long even = n / 2 + n % 2;
 
     return (power(5, even) * power(4, odd)) % mod;
