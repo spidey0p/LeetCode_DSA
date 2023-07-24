99bee1167792d473a1720cbb18b8de64e668b189
spidey0p <jyotiradityabiswal64@gmail.com>
2023-07-24 23:12:49 +0530

    Buy and Sell Stock - II

---
+++ D1/BestTime.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &prices)
+{
+    int profit = 0;
+    for (int i = 0; i < prices.size(); i++)
+    {
+        if (prices[i] > prices[i - 1])
+        {
+            profit += (prices[i] - prices[i - 1]);
+        }
+    }
+    return profit;
+}
