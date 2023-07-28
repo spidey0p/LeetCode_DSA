764f8c4a09fe91ff8c324ac9c67089445f4ee1f0
spidey0p <jyotiradityabiswal64@gmail.com>
2023-07-28 09:42:50 +0530

    pallindrome partition

--- Striver-recursion/PalliPartition.cpp
+++ Striver-recursion/PalliPartition.cpp
@@ -1,65 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-class Solution
+vector<vector<string>> partition(string s)
 {
-public:
-    vector<vector<string>> partition(string s)
-    {
-        vector<vector<string>> res;
-        vector<string> path;
-        partitionHelper(0, s, path, res);
-        return res;
-    }
+    vector<vector<string>> res;
+    vector<string> path;
+    fun(0, s, path, res);
+    return res;
+}
 
-    void partitionHelper(int index, string s, vector<string> &path,
-                         vector<vector<string>> &res)
-    {
-        if (index == s.size())
-        {
-            res.push_back(path);
-            return;
-        }
-        for (int i = index; i < s.size(); ++i)
-        {
-            if (isPalindrome(s, index, i))
-            {
-                path.push_back(s.substr(index, i - index + 1));
-                partitionHelper(i + 1, s, path, res);
-                path.pop_back();
-            }
-        }
-    }
-
-    bool isPalindrome(string s, int start, int end)
-    {
-        while (start <= end)
-        {
-            if (s[start++] != s[end--])
-                return false;
-        }
-        return true;
-    }
-};
-int main()
+void fun(int index, string s, vector<string> &path, vector<vector<string>> &res)
 {
-    string s = "aabb";
-    Solution obj;
-    vector<vector<string>> ans = obj.partition(s);
-    int n = ans.size();
-    cout << "The Palindromic partitions are :-" << endl;
-    cout << " [ ";
-    for (auto i : ans)
+    if (index == s.size())
     {
-        cout << "[ ";
-        for (auto j : i)
-        {
-            cout << j << " ";
-        }
-        cout << "] ";
+        res.push_back(path);
+        return;
     }
-    cout << "]";
-
-    return 0;
+    for (int i = index; i < s.size(); i++)
+    {
+        if (isPallindrom(s.substr(index, i - index + 1)))
+            ;
+        fun(i + 1, s, path, res);
+        path.pop_back();
+    }
+}
+bool isPallindrom(string s, int start, int end)
+{
+    while (start <= end)
+    {
+        if (s[start++] != s[end--])
+            return false;
+    }
+    return true;
 }
