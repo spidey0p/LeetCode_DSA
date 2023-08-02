9aabf099c2ddd0e726cc7b0bc212fa4706d3fced
spidey0p <jyotiradityabiswal64@gmail.com>
2023-08-02 20:34:38 +0530

    1092. Shortest Common Supersequence

---
+++ DP-Striver/1092ShortestCS.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string shortestCommonSupersequence(string str1, string str2)
+{
+    int m = str1.size();
+    int n = str2.size();
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    for (int i = 0; i <= m; i++)
+        dp[i][0] = 0;
+    for (int i = 0; i <= n; i++)
+        dp[0][i] = 0;
+
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (str1[i - 1] == str2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = 0 + max(dp[i][j - 1], dp[i - 1][j]);
+        }
+    }
+    int len = dp[m][n];
+    int i = m;
+    int j = n;
+    int index = len - 1;
+    string ans = "";
+    while (i > 0 && j > 0)
+    {
+        if (str1[i - 1] == str2[j - 1])
+        {
+            ans += str1[i - 1];
+            index--;
+            i--;
+            j--;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1])
+
+        {
+            ans += str1[i - 1];
+            i--;
+        }
+        else
+        {
+            ans += str2[j - 1];
+            j--;
+        }
+    }
+    while (i > 0)
+    {
+        ans += str1[i - 1];
+        i--;
+    }
+    while (j > 0)
+    {
+        ans += str2[j - 1];
+        j--;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
